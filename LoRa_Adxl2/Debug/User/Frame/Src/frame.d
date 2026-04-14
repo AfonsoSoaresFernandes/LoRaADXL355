b793a99374b5b75a2b692464211f0e808bcec9e3
Afonso Fernandes <afonsofernandes@solvit.pt>
2026-04-14 14:53:02 +0100

    delete trash

--- LoRa_Adxl2/Debug/User/Frame/Src/frame.d
+++
@@ -1,3 +0,0 @@
-User/Frame/Src/frame.o: ../User/Frame/Src/frame.c \
- C:/Users/afonsofernandes/STM32CubeIDE/Workspace_LORA/LoRa_Adxl2/User/Frame/Inc/frame.h
-C:/Users/afonsofernandes/STM32CubeIDE/Workspace_LORA/LoRa_Adxl2/User/Frame/Inc/frame.h:
--- LoRa_Adxl2/Debug/User/Queue/queue.d
+++
@@ -1,4 +0,0 @@
-User/Queue/queue.o: ../User/Queue/queue.c ../User/Queue/queue.h \
- ../User/Queue/../SM2/storagemanager.h
-../User/Queue/queue.h:
-../User/Queue/../SM2/storagemanager.h:
